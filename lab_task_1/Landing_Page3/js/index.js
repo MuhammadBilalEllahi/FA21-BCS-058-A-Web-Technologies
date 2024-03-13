c4298355965b4fdc18ceb77f2dce2b5fa931f083
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-03-14 00:04:13 +0500

    updated front landing page

--- lab_task_1/Landing_Page3/js/index.js
+++ lab_task_1/Landing_Page3/js/index.js
@@ -1,14 +1,17 @@
 
 
 const navbar = document.querySelector('.navbar');
+const navoCntainer = document.querySelector('.nav-container');
 
 
 window.onscroll = () => {
     if (window.scrollY > 300) {
         navbar.classList.add('scrolled');
+        navoCntainer.classList.add('scrolled-pad-less');
         console.log("here")
     } else {
         navbar.classList.remove('scrolled');
+        navoCntainer.classList.remove('scrolled-pad-less');
         console.log("hered")
     }
 };
