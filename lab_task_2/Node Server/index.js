d670d2c30c5a51b2f0df540098972a754dd129ca
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-03-27 01:54:43 +0500

    env

--- lab_task_2/Node Server/index.js
+++ lab_task_2/Node Server/index.js
@@ -1,4 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const server = express();
+const app = express();
+const PORT = 2211;
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection;
+
+db.on("error", (error)=>{console.error(error)});
+db.once("open", ()=>{console.log("Connected to database")});
+
+app.listen(PORT, ()=>{console.log(`server running on port ${PORT}`)})
