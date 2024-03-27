12ac25a45337c8aa59f933918a7776e14f20a207
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-03-27 13:04:02 +0500

    mongoose

--- lab_task_2/Node Server/index.js
+++ lab_task_2/Node Server/index.js
@@ -1,12 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const server = express();
 const PORT = 2211;
-mongoose.connect(process.env.DATABASE_URL)
+let Student = require("./models/Student.js")
+
+server.listen(PORT, ()=>{console.log(`server running on port ${PORT}`)})
+// process.env.DATABASE_URL
+// mongoose.connect("mongodb://localhost/StudentsDB")
+mongoose.connect("mongodb://localhost:27017/StudentsDB")
 const db = mongoose.connection;
+
 
 db.on("error", (error)=>{console.error(error)});
 db.once("open", ()=>{console.log("Connected to database")});
 
-app.listen(PORT, ()=>{console.log(`server running on port ${PORT}`)})
+server.get("/api/students", async function  (req,res){
+    let students = await Student.find();
+    // let students = [
+    //     {
+    //         name : "Abc", 
+    //         address: "Abcd"
+    //     },
+    //     {
+    //         name : "def", 
+    //         address: "defg"
+    //     }
+    // ]
+    res.send(students)
+})
+
+
+server.get("/api/students/:id", async function  (req,res){
+    let students = await Student.findById(req.params.id);
+    res.send(students)
+})
