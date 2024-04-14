592b33cdbf834207c9a3ad73d71876e5d1db07c8
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-15 01:43:47 +0500

    Routes Added

--- lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.jsx
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/home/Home.jsx';
 import Footer from './components/main/Footer.jsx';
 import Nav from './components/main/Nav.jsx';
+import ShopByCategory from './components/home/ShopByCategory.jsx';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Routes>
 
       <Route path='/' element={<Home/>}/>
+      <Route path='/standard-shop' element={<ShopByCategory/>}/>
     
     </Routes>
     <Footer/>
