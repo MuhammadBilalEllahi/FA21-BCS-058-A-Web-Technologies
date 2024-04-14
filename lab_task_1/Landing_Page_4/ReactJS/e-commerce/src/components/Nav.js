148cb1307507de2719300ff90b0bb65d9dbf6c79
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-15 00:09:35 +0500

    Refactor class names in Nav.js component

--- lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/components/Nav.js
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/components/Nav.js
@@ -3,21 +3,21 @@ import React from 'react'
 function Nav() {
   return (
     <>
-       <nav  class="navbar">
-        <div class="nav-container container">
-            <h5 class="nav-logo">Wooden</h5>
+       <nav  className="navbar">
+        <div className="nav-container container">
+            <h5 className="nav-logo">Wooden</h5>
 
 
-            <ul class="nav-ul">
-                <li class="nav-li"><a href="./index.html">Home</a> </li>
-                <li class="nav-li"><a href="./standard-shop.html">Shop</a> </li>
-                <li class="nav-li"> <a href="#Products">Products</a> </li>
-                <li class="nav-li"> <a href="#Blog">Blog</a> </li>
-                <li class="nav-li"> <a href="#Page">Page</a> </li>
+            <ul className="nav-ul">
+                <li className="nav-li"><a href="./index.html">Home</a> </li>
+                <li className="nav-li"><a href="./standard-shop.html">Shop</a> </li>
+                <li className="nav-li"> <a href="#Products">Products</a> </li>
+                <li className="nav-li"> <a href="#Blog">Blog</a> </li>
+                <li className="nav-li"> <a href="#Page">Page</a> </li>
             </ul>
 
 
-            <div class="icon-btn-container">
+            <div className="icon-btn-container">
              
             </div>
         </div>
