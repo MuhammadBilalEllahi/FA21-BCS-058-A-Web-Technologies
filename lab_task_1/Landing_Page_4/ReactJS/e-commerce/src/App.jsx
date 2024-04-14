5cde6d0869ad379b1ce25f5bd52436cc40941451
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-15 00:56:24 +0500

    JSX used instead of JS. Standardized

---
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './components/home/Home.jsx';
+import Footer from './components/main/Footer.jsx';
+import Nav from './components/main/Nav.jsx';
+
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Home/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
