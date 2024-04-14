290ad0d52ca32c8b02d9c20dbe23a9394cc9d27c
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-15 02:46:42 +0500

    "Page Not Found" Page  Added and Updated routing paths in App.jsx

--- lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.jsx
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 import Home from './components/home/Home.jsx';
 import Footer from './components/main/Footer.jsx';
 import Nav from './components/main/Nav.jsx';
-import ShopByCategory from './components/home/ShopByCategory.jsx';
+import Shop from './components/shop/Shop.jsx';
+import PageNotFound from './components/pagenotfound/PageNotFound.jsx';
+
 
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
     <Routes>
 
       <Route path='/' element={<Home/>}/>
-      <Route path='/standard-shop' element={<ShopByCategory/>}/>
+      <Route path='/standard-shop' element={<Shop/>}/>
+
+      <Route  path='*' element={<PageNotFound/>}/>
     
     </Routes>
     <Footer/>
---
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/components/pagenotfound/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div style={{
+        "display": "flex",
+        "justifyContent": "center",
+        "alignItems": "center",
+        "minHeight" : "50vh"
+    }}>
+      <h1>PAGE NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
