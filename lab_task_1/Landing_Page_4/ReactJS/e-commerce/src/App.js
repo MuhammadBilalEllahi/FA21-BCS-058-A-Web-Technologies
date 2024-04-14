5e84c9fc43d422231d0f638f483695cc1c006f45
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-15 00:00:10 +0500

    Add Nav component to App.js in ReactJS e-commerce app

--- lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.js
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
     <>
+    <Nav/>
     </>
   );
 }
---
+++ lab_task_1/Landing_Page_4/ReactJS/e-commerce/src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <>
+       <nav  class="navbar">
+        <div class="nav-container container">
+            <h5 class="nav-logo">Wooden</h5>
+
+
+            <ul class="nav-ul">
+                <li class="nav-li"><a href="./index.html">Home</a> </li>
+                <li class="nav-li"><a href="./standard-shop.html">Shop</a> </li>
+                <li class="nav-li"> <a href="#Products">Products</a> </li>
+                <li class="nav-li"> <a href="#Blog">Blog</a> </li>
+                <li class="nav-li"> <a href="#Page">Page</a> </li>
+            </ul>
+
+
+            <div class="icon-btn-container">
+             
+            </div>
+        </div>
+
+    </nav>
+    </>
+  )
+}
