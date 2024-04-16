ef53b25649be0213f060e4316d4e2d117b853e47
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-17 04:30:00 +0500

    font styles in Next

--- practice/NextJs/nextjs-test/app/layout.jsx
+++ practice/NextJs/nextjs-test/app/layout.jsx
@@ -1,10 +1,10 @@
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 
 // components
 import NavBar from "./component/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
 
         <NavBar/>
 
