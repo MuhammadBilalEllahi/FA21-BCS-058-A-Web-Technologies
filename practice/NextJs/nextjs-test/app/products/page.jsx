83cee1d287d2cc355ea7712bf0c97e41a364afd7
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-17 05:06:16 +0500

    Fetch is cached after 1st run. So, for realtime u have to tell next to refetch it In products.jsx

--- practice/NextJs/nextjs-test/app/products/page.jsx
+++ practice/NextJs/nextjs-test/app/products/page.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 
 
 async function getData(){
-  const res = await fetch("http://localhost:2211/api/products")
+  const res = await fetch("http://localhost:2211/api/products", {
+    next:{
+      revalidate: 30 // if web "visit"(not while being on page) after 30 sec then  refecth //BAD APPROACH I THINK
+    }
+  })
   return res.json()
+
+  // This is cached after 1st run. So, for realtime u have to tell next to refetch it
 
   // .then(
   //   (res)=> res.json()
