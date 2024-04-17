20cf79defada45aaea85086ec7716c2a3050bcb8
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-17 23:51:57 +0500

    Buffer is not needed to be imported in NExtJs

--- practice/NextJs/nextjs-test/app/products/page.jsx
+++ practice/NextJs/nextjs-test/app/products/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
@@ -30,20 +31,34 @@ export default  async function Products() {
 
   const products = await getData();
   console.log(products)
+  
   return (
-    <>
+    
+    <div className=' flex flex-wrap'>
     {products.map(
       (product) =>(
         
-        <div key={product._id}>
+        <div key={product._id} className=' w-60 m-2 p-2 border-teal-400 border'>
           <Link href={`/products/${product._id}`}>
+            <Image 
+            src={`data: ${product.p_img.contentType};base64,${ Buffer.from(product.p_img.data).toString('base64')}`}
+            // `data: ${product.p_img.contentType};base64,${ product.p_img.data.toString('base64')}`
+                        // Buffer.from(product.p_img.data).toString('base64')
+            alt={product.p_name}
+            width={200}
+            height={200}
+            
+            />
           <h1 >{product.p_name}</h1>
+          <p>Price: <strike className=" text-red-900">{product.p_orig_price}</strike>$</p>
+          <p>New Price: {product.p_sale_price}$</p>
           </Link>
         </div>
         
     )
     )}
-    </>
+    
+    </div>
   )
 }
 
