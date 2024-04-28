cc5aff6cfe4cf5113d5679963ea668cc27749fea
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-28 19:24:08 +0500

    ES Module

--- lab_task_1/Landing_Page_4/Server/connection.js
+++ lab_task_1/Landing_Page_4/Server/connection.js
@@ -1,9 +1,21 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Products")
-const db = mongoose.connection;
+
+
+import { connect, connection } from "mongoose";
+connect("mongodb://localhost:27017/Products")
+const db = connection;
 db.on("error", (error) => { console.error(error) });
 db.once("open", () => { console.log("Connected to database") });
 
-module.exports = {
+export default {
     db
 }
+
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/Products")
+// const db = mongoose.connection;
+// db.on("error", (error) => { console.error(error) });
+// db.once("open", () => { console.log("Connected to database") });
+
+// module.exports = {
+//     db
+// }
