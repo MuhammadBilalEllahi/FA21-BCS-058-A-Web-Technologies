89e006f0cbea6530495286064d5f51735ad526b5
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-28 20:10:11 +0500

    ES Module Removed

--- lab_task_1/Landing_Page_4/Server/connection.js
+++ lab_task_1/Landing_Page_4/Server/connection.js
@@ -1,21 +1,11 @@
-
-
-import { connect, connection } from "mongoose";
-connect("mongodb://localhost:27017/Products")
-const db = connection;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/Products")
+const db = mongoose.connection;
 db.on("error", (error) => { console.error(error) });
 db.once("open", () => { console.log("Connected to database") });
 
-export default {
+module.exports = {
     db
 }
 
-// const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost:27017/Products")
-// const db = mongoose.connection;
-// db.on("error", (error) => { console.error(error) });
-// db.once("open", () => { console.log("Connected to database") });
 
-// module.exports = {
-//     db
-// }
