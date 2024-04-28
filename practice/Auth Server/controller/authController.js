62af23b574c77e0e75275c7f68825a17d38f0b69
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-29 03:21:09 +0500

    JWT

--- practice/Auth Server/controller/authController.js
+++ practice/Auth Server/controller/authController.js
@@ -4,23 +4,53 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt')
 
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const fsPromises = require('fs').promises;
+const path = require('path')
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ "message": "username and password required" })
 
     const foundUser = usersDB.users.find(person => person.username === user);
     if (!foundUser) return res.sendStatus(401) //Unauthorized
-
-    const match  = await bcrypt.compare(pwd,foundUser.password)
-
-    if(match){
+    const match = await bcrypt.compare(pwd, foundUser.password)
+    if (match) {
         // create JWT's
-        res.json({"success": `User ${user} is Logged In!`})
-    }else
-    {
+        const accessToken = jwt.sign({
+            "username": foundUser.username
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "30s" }
+        );
+
+
+        const refreshToken = jwt.sign({
+            "username": foundUser.username
+        },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        //SAvign refresh token with current user
+
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username)
+
+        const currentUser = {...foundUser, refreshToken}
+
+        usersDB.setUsers([...otherUsers, currentUser]);
+
+        await fsPromises.writeFile(
+            path.join(__dirname, "..", "model", "users.json"),
+            JSON.stringify(usersDB.users)
+        )
+
+        res.json({ accessToken})
+    } else {
         res.sendStatus(401)
     }
 
 
 }
-module.exports ={handleLogin}
+module.exports = { handleLogin }
