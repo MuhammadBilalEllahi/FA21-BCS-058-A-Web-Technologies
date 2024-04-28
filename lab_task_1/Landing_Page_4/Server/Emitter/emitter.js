078b72c3e2bfcffd88a9081c725f354f4081151d
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-28 20:32:34 +0500

    Emitter

---
+++ lab_task_1/Landing_Page_4/Server/Emitter/emitter.js
@@ -0,0 +1,31 @@
+const fsPromises = require('fs').promises;
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
+// initialize object 
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        // const rawData = await fsPromises.readFile(
+        //     filePath,
+        //     !contentType.includes('image') ? 'utf8' : ''
+        // );
+        // const data = contentType === 'application/json'
+        //     ? JSON.parse(rawData) : rawData;
+        // response.writeHead(
+        //     filePath.includes('404.html') ? 404 : 200,
+        //     { 'Content-Type': contentType }
+        // );
+        // response.end(
+        //     contentType === 'application/json' ? JSON.stringify(data) : data
+        // );
+    } catch (err) {
+        console.log(err);
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.statusCode = 500;
+        response.end();
+    }
+}
