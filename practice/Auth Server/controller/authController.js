5d6072d37d0a4c6182e01e269cd47dfcc7848d9f
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-29 02:26:52 +0500

    handle Login authController

---
+++ practice/Auth Server/controller/authController.js
@@ -0,0 +1,26 @@
+const usersDB = {
+    users: require("../model/users.json"),
+    setUsers: function (data) { this.users = data }
+}
+const bcrypt = require('bcrypt')
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ "message": "username and password required" })
+
+    const foundUser = usersDB.users.find(person => person.username === user);
+    if (foundUser) return res.sendStatus(401) //Unauthorized
+
+    const match  = await bcrypt.compare(pwd,foundUser.password)
+
+    if(match){
+        // create JWT's
+        res.json({"success": `User ${user} is Logged In!`})
+    }else
+    {
+        res.sendStatus(401)
+    }
+
+
+}
+module.exports ={handleLogin}
