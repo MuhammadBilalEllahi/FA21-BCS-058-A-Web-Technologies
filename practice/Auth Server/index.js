a8a56e4cce77df5ccd7ca5972edf374e42b8efa4
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-29 02:43:11 +0500

    middlewares in auth

--- practice/Auth Server/index.js
+++ practice/Auth Server/index.js
@@ -4,6 +4,9 @@ const PORT = 1122;
 // const mongoDB = require('./db/connection')
 const cors = require('cors');
 let ejs_layout = require("express-ejs-layouts")
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOption');
 
 
 server.use(ejs_layout)
@@ -11,8 +14,13 @@ server.set("view engine", "ejs");
 server.use(express.json());
 server.use(express.static("public"))
 server.use(cors())
-server.listen(PORT, () => { console.log(`server running on port ${PORT}`) })
 
+
+
+// custom middleware logger
+server.use(logger);
+server.use(cors(corsOptions));
+server.use(express.json());
 
 
 const registerRouter = require("./routes/api/register")
@@ -28,5 +36,5 @@ server.get("*", async function (req, res) {
     res.send("Page Not Found")
 })
 
-
-
+server.use(errorHandler);
+server.listen(PORT, () => { console.log(`server running on port ${PORT}`) })
