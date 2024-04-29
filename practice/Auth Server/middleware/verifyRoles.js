024f4ea0a5f9944ed43acd115c3946ae8be07540
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-29 23:58:43 +0500

    verifyRoles mmiddleware

--- practice/Auth Server/middleware/verifyRoles.js
+++ practice/Auth Server/middleware/verifyRoles.js
@@ -5,6 +5,10 @@ const verifyRoles = {...allowedRoles =>{
         console.log(rolesArray)
         console.log(req.roles)
 
-        const result = req.roles.map (role=> rolesArray.includes(roles)).find(val => val ===true )
+        const result = req.roles.map (role=> rolesArray.includes(role)).find(val => val ===true )
+        if(!result) return res.sendStatus(401)
+        next();
     }
 }}
+
+module.exports = verifyRoles
