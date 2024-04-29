d3025cbf7405d0b8a2b4430c30ecd60ad9f2baf8
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-04-29 23:51:25 +0500

    roles in refreshController

--- practice/Auth Server/controller/refreshController.js
+++ practice/Auth Server/controller/refreshController.js
@@ -22,9 +22,13 @@ const handleRefreshToken = async (req, res) => {
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
             if (err || foundUser.username !== decoded.username) return res.sendStatus(403)
+            const roles = Object.values(foundUser.roles)
             const accessToken = jwt.sign(
                 {
-                    "username": decoded.username
+                    "UserInfo":{
+                        "username": decoded.username,
+                        "roles": roles
+                    }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: "30s" }
