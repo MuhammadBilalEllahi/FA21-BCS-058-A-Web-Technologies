28ae2f9da4dddbaaefb26c749cd78945f8601311
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-01 03:49:29 +0500

    replaced localhost to 0.0.0.0 to remove error >ServerSelectionError()

--- practice/E-Commerce-MERN/config/dbConnect.js
+++ practice/E-Commerce-MERN/config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnect = ()=>{
     try {
-        const con = mongoose.connect("mongodb://localhost:27017/ecom")
+        const con = mongoose.connect("mongodb://0.0.0.0:27017/ecom")
         console.log("Database Connected Successfully")
     } catch (error) {
         console.log(`Database has Error: ${error}`)
