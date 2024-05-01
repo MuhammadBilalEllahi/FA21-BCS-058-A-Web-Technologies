85cb673b77b6a75fc1a26dd135494fb66c163568
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-01 22:23:34 +0500

    is Pass correct bcrypt

--- practice/E-Commerce-MERN/models/userModel.js
+++ practice/E-Commerce-MERN/models/userModel.js
@@ -32,5 +32,9 @@ userSchema.pre("save",async function (next){
     this.password = await bcrypt.hash(this.password, salt)
 })
 
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword,this.password)
+}
+
 //Export the model
 module.exports = mongoose.model('User', userSchema);
