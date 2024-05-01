8896cafbbdc1dd8afb083a41b4855b72f5eecbd7
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-02 02:56:06 +0500

    user block itself

--- practice/E-Commerce-MERN/controller/userController.js
+++ practice/E-Commerce-MERN/controller/userController.js
@@ -54,11 +54,11 @@ const getAllUsers = async (req, res) => {
 
 const getaUser = async (req, res) => {
     // const user = await User.find()
-    const { id } = req.user
-    console.log(id)
+    const { _id } = req.user
+    console.log(_id)
 
     try {
-        const user = await User.findById(id);
+        const user = await User.findById(_id);
         res.json(
             {
                 user: user
@@ -72,11 +72,11 @@ const getaUser = async (req, res) => {
 
 const deleteaUser = async (req, res) => {
     // const user = await User.find()
-    const { id } = req.user
-    console.log(id)
+    const { _id } = req.user
+    console.log(_id)
 
     try {
-        const deleted_user = await User.findByIdAndDelete(id);
+        const deleted_user = await User.findByIdAndDelete(_id);
         res.json(
             {
                 deleted_user: deleted_user
@@ -88,29 +88,86 @@ const deleteaUser = async (req, res) => {
     }
 }
 
-const updateaUser = async (req, res) => {
+const updateaUser = asyncHandler(
+    async (req, res) => {
+        const { _id } = req.user
+        try {
+            const updated_user = await User.findByIdAndUpdate(_id, {
+    
+                firstname: req?.body?.firstname,
+                lastname: req?.body?.lastname,
+                email: req?.body?.email,
+                mobile: req?.body?.mobile,
+                password: req?.body?.password,
+    
+            },
+                {
+                    new: true
+                });
+            res.json(updated_user)
+    
+    
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+)
 
-    const { id } = req.user
-
-
+const blockUser = asyncHandler( async (req,res)=>{
+    const { _id } = req.user
     try {
-        const updated_user = await User.findByIdAndUpdate(id, {
-
-            firstname: req?.body?.firstname,
-            lastname: req?.body?.lastname,
-            email: req?.body?.email,
-            mobile: req?.body?.mobile,
-            password: req?.body?.password,
+        const blockeded_user = await User.findByIdAndUpdate(_id, {
+            isBlocked: true
 
         },
             {
                 new: true
             });
-        res.json(updated_user)
+        res.json({
+            message : "User Blocked"
+        })
 
 
     } catch (error) {
         throw new Error(error)
     }
-}
-module.exports = { createUser, loginUserController, getAllUsers, getaUser, deleteaUser, updateaUser }
+})
+const unBlockUser = asyncHandler( async (req,res)=>{
+    const { _id } = req.user
+    try {
+        const un_blockeded_user = await User.findByIdAndUpdate(_id, {
+            isBlocked: false
+
+        },
+            {
+                new: true
+            });
+        res.json({
+            message : "User unBlocked"
+        })
+
+
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+// const blockaUser = async (req, res) => {
+//     const { _id , isBlocked} = req.user
+//     try {
+//         const blockeded_user = await User.findByIdAndUpdate(_id, {
+//             isBlocked: !isBlocked
+//Iam not writing !Blocked because maybe if there isn't a block value or 
+// is not passed fue to an error or db didn't work, which it does work. anywasy just doing it
+//         },
+//             {
+//                 new: true
+//             });
+//         res.json(blockeded_user)
+
+
+//     } catch (error) {
+//         throw new Error(error)
+//     }
+// }
+
+module.exports = { createUser, loginUserController, getAllUsers, getaUser, deleteaUser, updateaUser , blockUser,unBlockUser }
--- practice/E-Commerce-MERN/routes/authRoute.js
+++ practice/E-Commerce-MERN/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {createUser, loginUserController, getAllUsers, getaUser, deleteaUser, updateaUser} = require("../controller/userController")
+const {createUser, loginUserController, getAllUsers, getaUser, deleteaUser, updateaUser, blockUser, unBlockUser} = require("../controller/userController")
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware")
 
 router.post("/register", createUser)
@@ -9,5 +9,8 @@ router.get("/get-users",getAllUsers)
 router.get("/get-a-user/:id",authMiddleware,isAdmin, getaUser)
 router.get("/delete-a-user/:id", deleteaUser)
 router.get("/update-a-user/:id", authMiddleware,updateaUser)
+
+router.get("/block-user/:id", authMiddleware,isAdmin,blockUser)
+router.get("/unblock-user/:id", authMiddleware,isAdmin,unBlockUser)
 
 module.exports = router;
