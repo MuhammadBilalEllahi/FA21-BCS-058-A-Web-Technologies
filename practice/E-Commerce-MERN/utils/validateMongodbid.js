0dcaf68170c2bb78b6ae23c03fad349759a4a7d3
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-02 03:04:22 +0500

    Code fix mongoose.Schema.Types is wrong mongoose.Types is right

--- practice/E-Commerce-MERN/utils/validateMongodbid.js
+++ practice/E-Commerce-MERN/utils/validateMongodbid.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const validateMongoDbId = (id) =>{
-    const isValid = mongoose.Schema.Types.ObjectId.isValid(id)
+    const isValid = mongoose.Types.ObjectId.isValid(id)
     if(!isValid) throw new Error("This id is not valid or Not Found")
 }
 module.exports = validateMongoDbId;
