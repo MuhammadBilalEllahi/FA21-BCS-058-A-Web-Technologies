b2eece3e6799fcf16ddc7c30d27193f7c3333ec3
MuhammadBilalEllahi <bilalillahi25@gmail.com>
2024-05-05 23:37:28 +0500

    isAdmin then delete update or create product

--- practice/E-Commerce-MERN/routes/productRoute.js
+++ practice/E-Commerce-MERN/routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router()
 const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require("../controller/productController");
+const { isAdmin } = require("../middlewares/authMiddleware");
 
 
-router.post("/",createProduct)
+router.post("/",isAdmin,createProduct)
 router.get("/:id",getaProduct)
 router.get("/",getAllProduct)
-router.post("/:id",updateProduct)
-router.get("/:id",deleteProduct)
+router.post("/:id",isAdmin,updateProduct)
+router.delete("/:id",isAdmin,deleteProduct)
 module.exports = router
