83f89579e807a08cfc6492e9901b1e39930f1149
MuhammadBilalEllahi <bilalillahi25@gmail.com>
2024-05-05 23:49:18 +0500

    auth first then admin. remember

--- practice/E-Commerce-MERN/routes/productRoute.js
+++ practice/E-Commerce-MERN/routes/productRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router()
 const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require("../controller/productController");
-const { isAdmin } = require("../middlewares/authMiddleware");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 
 
-router.post("/",isAdmin,createProduct)
+router.post("/",authMiddleware,isAdmin, createProduct)
 router.get("/:id",getaProduct)
 router.get("/",getAllProduct)
-router.post("/:id",isAdmin,updateProduct)
-router.delete("/:id",isAdmin,deleteProduct)
+router.post("/:id",authMiddleware,isAdmin, updateProduct)
+router.delete("/:id",authMiddleware,isAdmin, deleteProduct)
 module.exports = router
