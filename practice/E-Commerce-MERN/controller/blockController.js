71c81f322aaf1326adcab8c701b1860fcac435c6
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-09 22:34:26 +0500

    $inc: {numView: 1}  increements a value in databasse. Syntax of mongo

--- practice/E-Commerce-MERN/controller/blockController.js
+++ practice/E-Commerce-MERN/controller/blockController.js
@@ -6,13 +6,44 @@ const validateMongoDbId = require("../utils/validateMongodbid")
 const createBlog = asyncHandler(async (req,res)=>{
     try {
         const newBlog = await Blog.create(req.body)
-        res.json({
-            status: "success",
-            newBlog
-        })
+        res.json(newBlog)
     } catch (error) {
         throw new Error(error)
     }
 })
 
-module.exports = {createBlog}
+
+const updateBlog = asyncHandler(async (req,res)=>{
+    const {id} = req.params
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(id,req.body, {
+            new: true
+        })
+        res.json(updatedBlog)
+        // res.json({
+        //     status: "success",
+        //     newBlog
+        // })
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+const getBlog = asyncHandler( async (req,res)=>{
+    const { id} = req.params
+    try {
+        const getaBlog = await Blog.findById(id)
+         await Blog.findByIdAndUpdate(id,{
+            $inc: {numViews: 1}
+         }, {
+            new : true
+         })
+        res.json(getaBlog)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+module.exports = {createBlog,updateBlog, getBlog}
--- practice/E-Commerce-MERN/routes/blogRoute.js
+++ practice/E-Commerce-MERN/routes/blogRoute.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const { createBlog } = require("../controller/blockController")
+const { createBlog, updateBlog, getBlog } = require("../controller/blockController")
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware")
 const router = express.Router()
 
 
 router.post('/',authMiddleware,isAdmin,createBlog)
+router.put('/:id',authMiddleware,isAdmin,updateBlog)
+router.get('/get-blog/:id',getBlog)
+
+
 module.exports = router
