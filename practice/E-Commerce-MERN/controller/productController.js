1e40063c277a53acbdfa8f339b0e6307ee3b6c22
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-11 01:26:39 +0500

    always use await before Model

--- practice/E-Commerce-MERN/controller/productController.js
+++ practice/E-Commerce-MERN/controller/productController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require("express-async-handler")
 const Product = require("../models/ProductModel")
 const slugify = require("slugify")
+const User = require("../models/userModel")
 
-const createProduct = asyncHandler(async (req,res)=>{
-    
+const createProduct = asyncHandler(async (req, res) => {
+
     try {
-        if(req.body.title){
+        if (req.body.title) {
             req.body.slug = slugify(req.body.title)
         }
         const createNewProduct = await Product.create(req.body)
@@ -15,13 +16,13 @@ const createProduct = asyncHandler(async (req,res)=>{
     }
 })
 
-const updateProduct = asyncHandler(async (req,res)=>{
-    const {id} = req.params;
+const updateProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
     try {
-        if(re.body.title){
+        if (re.body.title) {
             req.body.slug = slugify(req.body.title)
         }
-        const updateaProduct = await Product.findOneAndUpdate({id},req.body,{
+        const updateaProduct = await Product.findOneAndUpdate({ id }, req.body, {
             new: true
         })
         res.json(updateaProduct)
@@ -30,10 +31,10 @@ const updateProduct = asyncHandler(async (req,res)=>{
     }
 })
 
-const deleteProduct = asyncHandler(async (req,res)=>{
-    const {id} = req.params;
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
     try {
-        if(re.body.title){
+        if (re.body.title) {
             req.body.slug = slugify(req.body.title)
         }
         const deleteaProduct = await Product.findOneAndDelete(id)
@@ -43,9 +44,9 @@ const deleteProduct = asyncHandler(async (req,res)=>{
     }
 })
 
-const getaProduct = asyncHandler(async (req,res)=>{
-    const {id} = req.params;
-    
+const getaProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
     try {
         const getOneProduct = await Product.findById(id)
         res.json(getOneProduct)
@@ -54,57 +55,57 @@ const getaProduct = asyncHandler(async (req,res)=>{
     }
 })
 
-const getAllProduct = asyncHandler(async (req,res)=>{
+const getAllProduct = asyncHandler(async (req, res) => {
     // console.log(req.query)// to search different items in db by addign ? in url and field name and value. it tells all those vales requestes
-    
+
     try {
         // Filtering
-        const queryObj = {...req.query}
-        const excludedField = ["page","sort","limit","fields"]
-        excludedField.forEach((el)=> delete queryObj[el]);
+        const queryObj = { ...req.query }
+        const excludedField = ["page", "sort", "limit", "fields"]
+        excludedField.forEach((el) => delete queryObj[el]);
         console.log(queryObj)
 
-        let queryStr= JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,(match) =>  `$${match}`)
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
         console.log(JSON.parse(queryStr));
 
         let query = Product.find(JSON.parse(queryStr))
         // TypeError: Assignment to constant variable. if const is used instead of let
 
-        
+
         // const getAllProducts = await Product.find()
         // Sorting
-        if(req.query.sort){
+        if (req.query.sort) {
             const sortBy = req.query.sort.split(',').join(' ')
             console.log(sortBy);
             query = query.sort(sortBy)
-        }else{
+        } else {
             query = query.sort('-createdAt')
         }
         //  in url "?sort=  -price" sort inverse, "?sort=   price" without - sort in less to greater val
 
 
         // Limiting the fields
-        if(req.query.fields){
+        if (req.query.fields) {
             const fields = req.query.fields.split(',').join(' ')
             query = query.select(fields)
 
-        }else{
+        } else {
             query = query.select('-__v')
         }
 
         // Pagination
         const page = req.query.page
         const limit = req.query.limit
-        const skip = (page-1)* limit
-        console.log(page,limit,skip) // not show e.g first 10 products if page is 2 and limit is 10. show 11th and onwards product from page 2 
+        const skip = (page - 1) * limit
+        console.log(page, limit, skip) // not show e.g first 10 products if page is 2 and limit is 10. show 11th and onwards product from page 2 
         query = query.skip(skip).limit(limit)
 
-        if(req.query.page) {
+        if (req.query.page) {
             const productCount = await Product.countDocuments()
-            if(skip >= productCount) throw new Error("This Page Doesnot Exists")            
+            if (skip >= productCount) throw new Error("This Page Doesnot Exists")
         }
-        else{
+        else {
 
         }
 
@@ -119,4 +120,52 @@ const getAllProduct = asyncHandler(async (req,res)=>{
 
 
 
-module.exports = {createProduct, getaProduct, getAllProduct,updateProduct, deleteProduct}
+
+
+
+
+
+
+
+
+const addToWishList = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+
+    const { prodId } = req.body;
+    try {
+        const user = await User.findById(_id)
+
+        const alreadyAdded = await user.wishlist?.find(
+            (id) => id.toString() === prodId
+        )
+
+        if (alreadyAdded) {
+
+            let user = await User.findOneAndUpdate(_id,
+                {
+                    $pull: { wishlist: prodId }
+                }, {
+                new: true
+            }
+            )
+            res.json(user)
+        }
+        else {
+
+            let user = await User.findOneAndUpdate(_id,
+                {
+                    $push: { wishlist: prodId }
+                }, {
+                new: true
+            }
+            )
+            res.json(user)
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+
+module.exports = { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, addToWishList }
--- practice/E-Commerce-MERN/routes/productRoute.js
+++ practice/E-Commerce-MERN/routes/productRoute.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router()
-const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require("../controller/productController");
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, addToWishList } = require("../controller/productController");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 
 
-router.post("/",authMiddleware,isAdmin, createProduct)
-router.get("/:id",getaProduct)
-router.get("/",getAllProduct)
-router.post("/:id",authMiddleware,isAdmin, updateProduct)
-router.delete("/:id",authMiddleware,isAdmin, deleteProduct)
+router.post("/", authMiddleware, isAdmin, createProduct)
+router.post("/:id", authMiddleware, isAdmin, updateProduct)
+
+router.put("/wishlist", authMiddleware, addToWishList)
+
+router.get("/:id", getaProduct)
+router.get("/", getAllProduct)
+
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct)
 module.exports = router
