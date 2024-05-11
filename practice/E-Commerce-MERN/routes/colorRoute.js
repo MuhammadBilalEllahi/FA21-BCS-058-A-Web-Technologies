90529ab4ef7d6afa1e3632d3af93a2a16c0b7e04
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-12 04:16:25 +0500

    Color Route Model and Controller added

---
+++ practice/E-Commerce-MERN/routes/colorRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { createColor, getAllColor, updateColor, deleteColor, getaColor } = require("../controller/colorController");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware")
+
+const router = express.Router()
+
+
+router.post("/", authMiddleware, isAdmin, createColor)
+
+router.put("/:id", authMiddleware, isAdmin, updateColor)
+
+router.get("/:id", getaColor)
+router.get("/", getAllColor)
+
+
+router.delete("/:id", authMiddleware, isAdmin, deleteColor)
+
+module.exports = router;
