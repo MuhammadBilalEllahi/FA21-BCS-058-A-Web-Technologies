007230267c5ba3f9d9b7915bc33825b856904efe
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-11 19:42:42 +0500

    cloudinary sharp and multer added

---
+++ practice/E-Commerce-MERN/middlewares/uploadImages.js
@@ -0,0 +1,3 @@
+const multer = require("multer")
+const sharp = require("sharp")
+const path = require("path")
---
+++ practice/E-Commerce-MERN/utils/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary = require("cloudinary")
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+
+const cloudinaryUploadImg = async (fileToUpload) => {
+    return new Promise((resolve) => {
+        cloudinary.uploader.upload(fileToUpload, (result) => {
+            resolve(
+                {
+                    url: result.secure_url
+                },
+                {
+                    resource_type: "auto"
+                }
+            )
+        })
+    })
+}
+
+module.exports = cloudinaryUploadImg
