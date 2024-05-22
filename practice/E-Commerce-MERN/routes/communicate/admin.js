f98b7d87c66828226c564133619700892069150b
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2024-05-22 12:23:15 +0500

    Product List, Add Product, sidebar   added in Admin Dashboard

--- practice/E-Commerce-MERN/routes/communicate/admin.js
+++ practice/E-Commerce-MERN/routes/communicate/admin.js
@@ -2,26 +2,42 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const router = express.Router();
 
+
 router.get("/", asyncHandler(async (req, res) => {
-    res.render("src/index", { layout: "layouts/adminLayout" })
+    res.render("src/index", { layout: "layouts/adminLayout", title: "Dashboard" })
 }))
 
 
 router.get("/register", asyncHandler(async (req, res) => {
-    res.render("src/auth/registerAdmin", { layout: "layouts/adminLayout" })
+    res.render("src/auth/registerAdmin", { layout: "extras/adminLayout", title: "Register" })
 }))
 
 router.get("/login", asyncHandler(async (req, res) => {
-    res.render("src/auth/loginAdmin", { layout: "layouts/adminLayout" })
+    res.render("src/auth/loginAdmin", { layout: "extras/adminLayout", title: "Login" })
 }))
 
 router.get("/forgot-password", asyncHandler(async (req, res) => {
-    res.render("src/auth/forgotPassAdmin", { layout: "layouts/adminLayout" })
+    res.render("src/auth/forgotPassAdmin", { layout: "extras/adminLayout", title: "Forgot Password" })
 }))
 
 
+router.get('/add-product', (req, res) => {
+
+    res.render('src/pages/addProduct');
+});
+
+
+// router.get("/", asyncHandler(async (req, res) => {
+//     res.render("src/auth/forgotPassAdmin", { layout: "layouts/adminLayout" })
+// }))
 
 
 
+
+
+
+// router.get("/", asyncHandler(async (req, res) => {
+//     res.render("src/index", { layout: "layouts/adminLayout" })
+// }))
 
 module.exports = router 
